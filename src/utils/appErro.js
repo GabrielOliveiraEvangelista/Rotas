984125dfe5c5029b34e0a0dc9af60ffbc2a828ac
Gabriel Oliveira <evangelistago@gmail.com>
2023-09-29 15:05:00 -0300

    Study routes

---
+++ src/utils/appErro.js
@@ -0,0 +1,9 @@
+class appErro {
+    message;
+    statusCode;
+    constructor(message, statusCode = 400) {
+        this.message = message
+        this.statusCode = statusCode
+    }
+}
+module.exports = appErro
